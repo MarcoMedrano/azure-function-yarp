93c89fc4b27954c6ea5aae57d493deec0d456be3
Marco Medrano <marco.medrano.garay@gmail.com>
2022-11-05 10:38:10 -0400

    feat: yarp is being called.

--- Proxy.cs
+++ Proxy.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -23,21 +25,33 @@ public static class Proxy
         _client = server.CreateClient();
     }
 
-    private static TestServer CreateServer(string functionPath) =>
-        new TestServer(WebHost
-            .CreateDefaultBuilder()
-            // .ConfigureAppConfiguration((builderContext, config) =>
-            // {
-            //     System.Console.WriteLine("ENV " + builderContext.HostingEnvironment.EnvironmentName);
-            //     System.Console.WriteLine("PAth " + functionPath);
-            //     config
-            //         .SetBasePath(functionPath)
-            //         .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-            //         .AddJsonFile($"appsettings.{builderContext.HostingEnvironment.EnvironmentName}.json",
-            //             optional: true, reloadOnChange: true)
-            //         .AddEnvironmentVariables();
-            // })
-            .UseStartup<Startup>());
+    private static TestServer CreateServer(string functionPath)
+    {
+        var builder = WebApplication.CreateBuilder();
+        builder.WebHost.UseTestServer();
+        builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+        var app = builder.Build();
+        app.MapReverseProxy();
+        app.RunAsync();
+        return app.GetTestServer();
+        // WebApplicationFactory
+// new TestServer();
+//     // var builder2 = WebHost.CreateDefaultBuilder();
+//     //     builder2.
+//         new TestServer(
+//             // .ConfigureAppConfiguration((builderContext, config) =>
+//             // {
+//             //     System.Console.WriteLine("ENV " + builderContext.HostingEnvironment.EnvironmentName);
+//             //     System.Console.WriteLine("PAth " + functionPath);
+//             //     config
+//             //         .SetBasePath(functionPath)
+//             //         .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+//             //         .AddJsonFile($"appsettings.{builderContext.HostingEnvironment.EnvironmentName}.json",
+//             //             optional: true, reloadOnChange: true)
+//             //         .AddEnvironmentVariables();
+//             // })
+//             .UseStartup<Startup>());
+    }
 
     [FunctionName("Proxy")]
     public static Task<HttpResponseMessage> Run([HttpTrigger(
